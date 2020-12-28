b4653774e792a24329fa10993fc78104665eb12e
PedroGomes15 <pedro.armando15@hotmail.com>
2020-12-28 18:26:54 -0300

    Add more components

---
+++ Runtime/SceneLoadManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Photon.Pun;
+namespace Dragontailgames.Utils
+{
+    public class SceneLoadManager : MonoBehaviour
+    {
+        public static SceneLoadManager instance = null;
+        
+        void Awake()
+        {
+            if (instance == null)instance = this;
+            else if (instance != this)Destroy(gameObject);
+            DontDestroyOnLoad(gameObject);
+        }
+
+        public void gotoMenu()
+        {
+            var objects = FindObjectsOfType<GameObject>();
+
+            foreach (var aux in objects)
+            {
+                if(aux.gameObject != this.gameObject)
+                Destroy(aux.gameObject);
+            }
+            SceneManager.LoadScene(0);
+        }
+
+        public void gotoNext()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+
+        public void gotoScene(int index)
+        {
+            SceneManager.LoadScene(index);
+        }
+
+        public void reload()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+
+        public string GetCurrentScene()
+        {
+            return SceneManager.GetActiveScene().name;
+        }
+
+        public void Close()
+        {
+            Application.Quit();
+        }
+    }
+}
---
+++ Runtime/SoundManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Dragontailgames.Utils
+{
+    public class SoundManager : MonoBehaviour
+    {
+        // Componentes de Audio
+        public AudioSource EffectsSource;
+        public AudioSource MusicSource;
+        public AudioClip[] FXCliplist;
+    
+
+        // Intancia do Singleton.
+        public static SoundManager Instance = null;
+
+        // Iniciar instancia.
+        private void Awake()
+        {
+            // Se não houver instancia
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+            //Se já houver instancia.
+            else if (Instance != this)
+            {
+                Destroy(gameObject);
+            }
+
+            //Setar como dontdestroyonload
+            DontDestroyOnLoad(gameObject);
+
+            FXCliplist = Resources.LoadAll<AudioClip>("Audio/GAME_SFX");
+        }
+
+        // Toca SFX
+        public void Play(string clipname)
+        {
+            foreach ( AudioClip searchlip in FXCliplist)
+            {
+                if (searchlip.name == clipname)
+                {
+                    EffectsSource.clip = searchlip;
+                    EffectsSource.Play();
+                }
+            }
+        
+        }
+
+        // Toca a musica
+        public void PlayMusic(AudioClip clip)
+        {
+            MusicSource.clip = clip;
+            MusicSource.Play();
+        }
+    }
+}
